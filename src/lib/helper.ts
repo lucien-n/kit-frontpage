28ae546ce3042eb311e2964b2c61a5e2de8e9d28
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-22 15:44:58 +0200

    ref: changed date formatting

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -3,16 +3,18 @@ import type { RepoInfo } from '$lib/types/repo_info';
 export function formatDate(time: number | string): string {
 	const date = new Date(time);
 
-	const month = date.toLocaleString('default', { month: 'short' });
-	const day = date.getDate();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
+	const datestring =
+		date.getDate() +
+		'/' +
+		(date.getMonth() + 1) +
+		'/' +
+		date.getFullYear() +
+		' ' +
+		date.getHours() +
+		':' +
+		date.getMinutes();
 
-	const formatted_date = `${month} ${day} ${hours.toString().padStart(2, '0')}h${minutes
-		.toString()
-		.padStart(2, '0')}`;
-
-	return formatted_date;
+	return datestring;
 }
 
 export const getRepoInfo = async (repo: string, branch: string): Promise<RepoInfo | null> => {
