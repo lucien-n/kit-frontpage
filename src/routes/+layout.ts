38e32df06f9f1586621fb0242099d22e5c943e17
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-22 16:04:43 +0200

    lib: added vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,8 @@
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
