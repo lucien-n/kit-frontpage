6c3f51c1288c70f245364dc0b61cd96653076286
Lucien <lucien.neuhoff@protonmail.com>
2024-03-22 17:30:46 +0100

    glass effect card

--- src/lib/projects/projects.svelte
+++ src/lib/projects/projects.svelte
@@ -3,7 +3,10 @@
 	import * as Accordion from '$ui/accordion';
 </script>
 
-<Accordion.Root value="frontpage" class="w-full">
+<Accordion.Root
+	value="frontpage"
+	class="w-full rounded-md border border-primary/20 bg-background/20 px-12 py-8 shadow-sm backdrop-blur-3xl"
+>
 	<Project name="frontpage" />
 	<Project name="leptitcoinV2" />
 	<Project name="windows" />
