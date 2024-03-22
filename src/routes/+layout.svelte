8930d729eb633c87ded3a3a2aecbed85ab3ff818
Lucien <lucien.neuhoff@protonmail.com>
2024-03-22 14:38:25 +0100

    feat: added lightswitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
 <script>
 	import { Toaster } from '$ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
+	import Lightswitch from '$lib/lightswitch.svelte';
 </script>
 
 <svelte:head>
 	<title>Lucien Neuhoff</title>
 </svelte:head>
 
+<ModeWatcher />
 <Toaster richColors />
+
+<div class="absolute right-2 top-2 z-50">
+	<Lightswitch />
+</div>
 
 <main>
 	<slot />
