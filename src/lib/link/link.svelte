f7d4a93c9ab63e176118ad392cc7c63783729fec
Lucien <lucien.neuhoff@protonmail.com>
2024-03-22 16:56:29 +0100

    added icon on external link

--- src/lib/link/link.svelte
+++ src/lib/link/link.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$ui/button';
+	import { ExternalLink } from 'radix-icons-svelte';
 	import { links } from '.';
 
 	export let link: keyof typeof links;
@@ -7,5 +8,6 @@
 </script>
 
 <Button {href} variant="link" class="h-fit  p-0 text-xs " target="_blank">
-	<u>{label}</u>
+	<ExternalLink size={12} />
+	<u class="ml-1">{label}</u>
 </Button>
