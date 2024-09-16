019b34850a9f3a7eb879f7998b590d2fed5505c6
Lucien <lucien.neuhoff@protonmail.com>
2024-09-16 23:30:59 +0200

    removed sonner and toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { Toaster } from '&/sonner';
 	import '../app.pcss';
 	import { page } from '$app/stores';
 	import { slide } from 'svelte/transition';
@@ -8,8 +7,6 @@
 <svelte:head>
 	<title>Lucien Neuhoff</title>
 </svelte:head>
-
-<Toaster richColors />
 
 {#key $page.url.href}
 	<div class="h-full w-full" in:slide={{ axis: 'y', duration: 200 }}>
