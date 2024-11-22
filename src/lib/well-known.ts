be28af9edf1e9689714435b82b7014243d3ddca9
Lucien <contact@lucienn.dev>
2024-11-22 09:34:37 +0100

    refactor: :fire: removed discord wk

--- src/lib/well-known.ts
+++ src/lib/well-known.ts
@@ -1,6 +1,5 @@
-import { WK_ATPROTO_DID, WK_DISCORD } from '$env/static/private';
+import { WK_ATPROTO_DID } from '$env/static/private';
 
 export const WELL_KNOWN = {
-	'atproto-did': WK_ATPROTO_DID,
-	discord: WK_DISCORD
+	'atproto-did': WK_ATPROTO_DID
 } as const;
